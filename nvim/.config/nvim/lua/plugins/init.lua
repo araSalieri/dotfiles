d21fffda4ad749921ca47c94f6a5deab6eda415f
araSalieri <rakhawiswara@gmail.com>
2026-04-21 00:11:28 +0700

    feat: auto open debugger ui when start

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -411,21 +411,25 @@ return {
               { id = "repl",        size = 0.15 },
             },
             size = 40,
-            position = "left",
+            position = "right",
           },
         },
       })
 
-      local codelldb_path     = vim.fn.stdpath("data") ..
+      local codelldb_path                                   = vim.fn.stdpath("data") ..
           "/mason/packages/codelldb/extension/adapter/codelldb"
 
-      dap.adapters.codelldb   = {
+      dap.adapters.codelldb                                 = {
         type       = "server",
         port       = "${port}",
         executable = { command = codelldb_path, args = { "--port", "${port}" } },
       }
 
-      dap.configurations.rust = {
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"]     = function() dapui.close() end
+
+      dap.configurations.rust                               = {
         {
           name        = "Launch",
           type        = "codelldb",
