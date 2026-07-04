c9692358b8dd4be926d8f3b7e07149043cfed3fb
araSalieri <rakhawiswara@gmail.com>
2026-07-04 17:38:31 +0700

    feat(nvim): add markdown-preview with keymaps

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -695,6 +695,26 @@ return {
   },
 
   {
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    ft = { "markdown" },
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreview<cr>",     ft = "markdown", desc = "Markdown Preview" },
+      { "<leader>mr", "<cmd>MarkdownPreviewRefresh<cr>", ft = "markdown", desc = "Markdown Preview Refresh" },
+      { "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", ft = "markdown", desc = "Markdown Preview Stop" },
+    },
+    config = function()
+      require("markdown_preview").setup({
+        instance_mode = "takeover",
+        port = 0,
+        open_browser = true,
+        default_theme = "dark",
+        debounce_ms = 300,
+      })
+    end,
+  },
+
+  {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
