d09e2efe00a3af6ea85ef10096ef4a626de15660
araSalieri <rakhawiswara@gmail.com>
2026-09-05 17:26:34 +0700

    refactor(fish): reworked config to source cachyos and trim customizations
    - Sourced /usr/share/cachyos-fish-config and dropped caelestia, direnv, zoxide, and starship initialization.
    - Moved mise activation to top-level and added a tmux wrapper naming sessions after the current directory.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,23 @@
-fish_add_path ~/.local/bin ~/bin ~/.npm-global/bin
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+function fish_greeting
+end
+
+function tmux
+    if test (count $argv) -eq 0; and not set -q TMUX
+        set -l name (basename $PWD | string replace -a -r '[:.\s]' '-')
+        command tmux new-session -A -s $name
+    else
+        command tmux $argv
+    end
+end
+
+mise activate fish | source
 
 if status is-interactive
-    # Suppress fish greeting
-    set -g fish_greeting ''
+    alias vim nvim
+    alias vi nvim
 
-    # FZF
     set -x FZF_CTRL_T_COMMAND 'fd -H . ~ -E node_modules -E target -E .git -E .venv -E dist'
     set -x FZF_CTRL_T_OPTS "
   --walker-skip .git,node_modules,target
@@ -21,74 +34,5 @@ if status is-interactive
     set -x FZF_ALT_C_OPTS "
   --preview 'tree -C {}'"
 
-    # Better LS
-    command -v eza &>/dev/null && alias ls='eza --icons --group-directories-first -1'
-
-    # Aliases
-    alias vim nvim
-    alias vi nvim
-
-    # Abbrs
-    abbr lg lazygit
-    abbr gd 'git diff'
-    abbr ga 'git add .'
-    abbr gc 'git commit -am'
-    abbr gl 'git log'
-    abbr gs 'git status'
-    abbr gst 'git stash'
-    abbr gsp 'git stash pop'
-    abbr gp 'git push'
-    abbr gpl 'git pull'
-    abbr gsw 'git switch'
-    abbr gsm 'git switch main'
-    abbr gb 'git branch'
-    abbr gbd 'git branch -d'
-    abbr gco 'git checkout'
-    abbr gsh 'git show'
-
-    abbr l ls
-    abbr ll 'ls -l'
-    abbr la 'ls -a'
-    abbr lla 'ls -la'
-
-    # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
-
-    # For jumping between prompts in foot terminal
-    function mark_prompt_start --on-event fish_prompt
-        echo -en "\e]133;A\e\\"
-    end
-
-    # tmux: name new sessions after the directory they're started in
-    function tmux
-        if test (count $argv) -eq 0; and not set -q TMUX
-            set -l name (basename $PWD | string replace -a -r '[:.\s]' '-')
-            command tmux new-session -A -s $name
-        else
-            command tmux $argv
-        end
-    end
-
-    # Auto-accept the omp commit split-plan confirmation ("Proceed? (y/N):")
-    function omp
-        if test (count $argv) -ge 1; and test "$argv[1]" = commit
-            printf 'y\n' | command omp $argv
-        else
-            command omp $argv
-        end
-    end
-
-    # Custom fish config (caelestia)
-    set -q XDG_CONFIG_HOME && set -l cConf $XDG_CONFIG_HOME/caelestia || set -l cConf $HOME/.config/caelestia
-    source $cConf/user-config.fish 2>/dev/null
-
-    # Init tools
-    command -v direnv &>/dev/null && direnv hook fish | source
-    command -v zoxide &>/dev/null && zoxide init fish --cmd cd | source
-    command -v starship &>/dev/null && starship init fish | source
     fzf --fish | source
 end
-
-mise activate fish | source
-kubectl completion fish | source
-
